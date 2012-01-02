3d5537a47152a861760d93a18b271a937b505fad
Alex Palcuie <alex.palcuie@gmail.com>
2012-01-03 00:27:33 +0200

    finished ch4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Return the title
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
 end
